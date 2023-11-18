f548c48966aeb24c0d940c760868cc66565f0945
Fred <fredericb.cv@gmail.com>
2023-11-19 00:52:59 +0100

    Fix tests script

--- tests.py
+++ tests.py
@@ -9,13 +9,13 @@ def generic_test(filename,num_tuple,num_value,print_matrix=True):
     # sudoku_obj.print_matrix()
     # sudoku_obj.print_possibility()
     # print('----')
-    sudoku_obj.solver()
-    if sudoku_obj.get_cell(num_tuple).value == num_value: 
+    sudoku_obj.run()
+    if sudoku_obj.matrix[num_tuple[0]*9+num_tuple[1]].value == num_value: 
         print('  => OK')
     else: 
         print('  => KO')
 
-    # sudoku_obj.print_matrix()
+    sudoku_obj.print_matrix()
     # sudoku_obj.print_possibility()
     # sudoku_obj.print_remain_values()
     # for tmp_couple in possibility_dict.keys():
@@ -53,11 +53,11 @@ if __name__ == '__main__':
     print('TEST9: Triplet')
     generic_test("./tests/example_triplet.json",(0,2),1)
 
-    # print('TEST10: Combinations1')
-    # generic_test("./tests/example_combinations1.json",(6,8),2)
+    print('TEST10: Combinations1')
+    generic_test("./tests/example_combinations1.json",(6,8),2)
 
-    # print('TEST11: Combinations2')
-    # generic_test("./tests/example_combinations2.json",(8,6),8)
+    print('TEST11: Combinations2')
+    generic_test("./tests/example_combinations2.json",(8,6),8)
 
-    # print('TEST12: Combinations3')
-    # generic_test("./tests/example_combinations3.json",(0,8),1)
+    print('TEST12: Combinations3')
+    generic_test("./tests/example_combinations3.json",(0,8),1)
